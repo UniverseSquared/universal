ec8900c85708bc21c7323db30074d3c46d7a410e
UniverseSquared <universesquared4@gmail.com>
2019-09-15 18:53:42 +0100

    Add read option to flash.

--- bin/flash.lua
+++ bin/flash.lua
@@ -4,34 +4,68 @@ local args = {...}
 local eeprom = component.proxy(component.list("eeprom")())
 local fs = component.proxy(computer.getBootAddress())
 
-if #args < 1 then
-    term.print("Usage: flash <path> [eeprom data]")
+function printUsage()
+    term.print("Usage: flash [-r/-w] <path> [eeprom data]")
+end
+
+if #args < 2 then
+    printUsage()
     return
 end
 
-local path = path.stringify(path.parse(args[1]))
-local data = args[2]
+local mode = args[1]
+local path = path.stringify(path.parse(args[2]))
 
-local handle, reason = fs.open(path)
-if not handle then
-    term.print(path .. ": failed to open (" .. reason .. ")")
+if mode == "-r" then
+    local handle, reason = fs.open(path, 'w')
+    if not handle then
+        term.print(path .. ": failed to open (" .. reason .. ")")
+        return
+    end
+
+    local data = eeprom.get()
+
+    if not fs.write(handle, data) then
+        term.print(path .. ": failed to write data")
+        fs.close(handle)
+        return
+    end
+
+    fs.close(handle)
+elseif mode == "-w" then
+    local data = args[3]
+
+    local handle, reason = fs.open(path)
+    if not handle then
+        term.print(path .. ": failed to open (" .. reason .. ")")
+        return
+    end
+
+    local buffer = ""
+    repeat
+        local data = fs.read(handle, math.huge)
+        buffer = buffer .. (data or "")
+    until data == nil
+
+    fs.close(handle)
+
+    term.print("Flashing. Do not power off or reboot.")
+
+    eeprom.set(buffer)
+
+    if data ~= nil then
+        eeprom.setData(data)
+    end
+
+    term.print("Done.")
+
+    term.print("Input a label for the eeprom, or nothing to leave it unchanged:")
+    local label = term.read()
+
+    if #label > 0 then
+        eeprom.setLabel(label)
+    end
+else
+    printUsage()
     return
 end
-
-local buffer = ""
-repeat
-    local data = fs.read(handle, math.huge)
-    buffer = buffer .. (data or "")
-until data == nil
-
-fs.close(handle)
-
-term.print("Flashing. Do not power off or reboot.")
-
-eeprom.set(buffer)
-
-if data ~= nil then
-    eeprom.setData(data)
-end
-
-term.print("Done. It is now safe to reboot.")
