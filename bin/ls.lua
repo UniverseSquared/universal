070bf78b73103add9c0d30628ee97304602de8ff
UniverseSquared <universesquared4@gmail.com>
2019-09-15 19:07:17 +0100

    Add -a option to ls.

--- bin/ls.lua
+++ bin/ls.lua
@@ -1,11 +1,22 @@
 local term = require("term")
 local path = require("path")
+local args = {...}
 local fs = component.proxy(computer.getBootAddress())
 local cwd = path.stringify(_OSENV.cwd)
 local list = fs.list(cwd)
 
-term.print(list.n .. " items")
+local showHidden = args[1] == "-a" or args[1] == "--all"
+
+local filteredList = {}
 
 for _, file in ipairs(list) do
+    if file:sub(1, 1) ~= '.' or showHidden then
+        table.insert(filteredList, file)
+    end
+end
+
+term.print(#filteredList .. " items")
+
+for _, file in ipairs(filteredList) do
     term.print(file)
 end
