0ddb52ae681aa9af4efdf7af0bc3de8e7785c371
UniverseSquared <universesquared4@gmail.com>
2019-09-21 00:18:19 +0100

    Show unit and filesystem label in lsfs.

--- bin/lsfs.lua
+++ bin/lsfs.lua
@@ -5,9 +5,11 @@ for fs in component.list("filesystem") do
     local used = fs.spaceUsed()
     local total = fs.spaceTotal()
     local percentage = used / total
+    local label = fs.getLabel()
+    label = label and " (" .. label .. ")" or ""
 
     term.print(string.format(
-        "%s - %.2f/%.2f used (%.3f%%)",
-        fs.address:sub(1, 8), used / 1024, total / 1024, percentage
+        "%s%s - %.2f/%.2f KiB used (%.3f%%)",
+        fs.address:sub(1, 8), label , used / 1024, total / 1024, percentage
     ))
 end
